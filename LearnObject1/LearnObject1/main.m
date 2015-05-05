5b000fe7f1c77d51604dc74da1ae935dfcd140f3
tao1993 <2372851048@qq.com>
2015-05-06 02:32:24 +0800

    init
    iniy

---
+++ LearnObject1/LearnObject1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnObject1
+//
+//  Created by tusm on 15/5/6.
+//  Copyright (c) 2015年 tao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
